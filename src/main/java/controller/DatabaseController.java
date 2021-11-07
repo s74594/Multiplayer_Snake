50605452aa4583831c304a81f7972a3b2a3cf3ef
Daniel Riel <>
2021-11-07 12:59:59 +0100

    UML diagramm updated

--- src/main/java/controller/DatabaseController.java
+++ src/main/java/controller/DatabaseController.java
@@ -3,18 +3,19 @@ package controller;
 import java.sql.*;
 
 public class DatabaseController {
-	
+
 	@SuppressWarnings("exports")
 	public static Connection Connector() {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			/**
 			 * getConnection("jdbc:sqlite:________.sqlite");
-			 *                               ^
-			 *                               |
-			 * insert database path here, otherwise an empty playerDB is being created
+			 * 								  ^
+			 * 								  |
+			 * 								  |- insert database path here,
+			 * otherwise an empty playerDB is being created
 			 */
-			Connection connection=DriverManager.getConnection("jdbc:sqlite:SQL Lite Database/playerDB.sqlite");
+			Connection connection = DriverManager.getConnection("jdbc:sqlite:SQL Lite Database/playerDB.sqlite");
 			return connection;
 		} catch (Exception e) {
 			// TODO: handle exception
