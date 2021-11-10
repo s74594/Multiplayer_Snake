6a0bb082c024564fddc4156296509c293a68da3e
Andreas Lehmann <alehmann42@gmail.com>
2021-11-10 18:08:33 +0100

    DEMO ready

--- src/main/java/com/example/multiplayer_snake/Snake.java
+++ src/main/java/com/example/multiplayer_snake/Snake.java
@@ -1,8 +1,6 @@
 package com.example.multiplayer_snake;
 
-import controller.ArenaController;
 import javafx.application.Application;
-import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
@@ -18,32 +16,32 @@ import java.net.URL;
 
 public class Snake extends Application {
 
-        @SuppressWarnings("exports")
-        @Override
-        public void start(Stage stage) throws IOException {
-            try {
-                URL url = new File("src/main/resources/com/example/multiplayer_snake/arenaView.fxml").toURI().toURL();
-                Parent rootParent = FXMLLoader.load(url);
-                Scene scene = new Scene(rootParent);
-                stage.setTitle("Snake");
-                stage.setResizable(false);
-                stage.setScene(scene);
-                stage.show();
+    @SuppressWarnings("exports")
+    @Override
+    public void start(Stage stage) throws IOException {
+        try {
+            Parent rootParent = FXMLLoader.load(getClass().getResource("loginView.fxml"));
+            Scene scene = new Scene(rootParent);
+            stage.setTitle("Snake");
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
 
-                //centerWindowOnScreen(stage);
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-        }
-
-        void centerWindowOnScreen(Stage stage) {
-            /* Center Snake Window on Screen */
-            Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
-            stage.setX((primScreenBounds.getWidth() - stage.getWidth()) / 2);
-            stage.setY((primScreenBounds.getHeight() - stage.getHeight()) / 2);
-        }
-
-        public static void main(String[] args) {
-            Application.launch(args);
+            centerWindowOnScreen(stage);
+        } catch (Exception e) {
+            // handle exception
+            System.err.println(e.getMessage());
         }
     }
+
+    void centerWindowOnScreen(Stage stage) {
+        /* Center Snake Window on Screen */
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+        stage.setX((primScreenBounds.getWidth() - stage.getWidth()) / 2);
+        stage.setY((primScreenBounds.getHeight() - stage.getHeight()) / 2);
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
--- src/main/java/controller/ArenaController.java
+++ src/main/java/controller/ArenaController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import com.example.multiplayer_snake.model.Player;
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,14 +10,17 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.File;
 import java.net.URL;
@@ -97,17 +102,35 @@ public class ArenaController {
 
     @FXML
     void snakeSteering(KeyEvent keyEvent) {
-        final double snakeSpeed = 2; // speed adjust
+        final double snakeSpeed = 3; // speed adjust
         double snakeX = snake.getLayoutX();
         double snakeY = snake.getLayoutY();
+
+        /*Path path = new Path();
+        path.getElements().add(new MoveTo(snake.getLayoutX(), snake.getLayoutY()));
+        path.getElements().add(new LineTo(0, 200));*/
 
         System.out.println("Key: " + keyEvent.getCode() + "  SnakeX: " + snakeX + "  SnakeY: " + snakeY);  // Debug
 
         switch (keyEvent.getCode()) {
-            case UP -> snake.setLayoutY(snakeY - snakeSpeed);
-            case DOWN -> snake.setLayoutY(snakeY + snakeSpeed);
-            case LEFT -> snake.setLayoutX(snakeX - snakeSpeed);
-            case RIGHT -> snake.setLayoutX(snakeX + snakeSpeed);
+            case UP -> {
+                snake.setLayoutY(snakeY - snakeSpeed);
+            }
+            case DOWN -> {
+                snake.setLayoutY(snakeY + snakeSpeed);
+            }
+            case LEFT -> {
+                snake.setLayoutX(snakeX - snakeSpeed);
+            }
+            case RIGHT -> {
+                snake.setLayoutX(snakeX + snakeSpeed);
+            }
         }
+        /*PathTransition pathTransition = new PathTransition();
+        pathTransition.setDuration(Duration.millis(6000));
+        pathTransition.setPath(path);
+        pathTransition.setNode(snake);
+        pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        pathTransition.play();*/
     }
 }
